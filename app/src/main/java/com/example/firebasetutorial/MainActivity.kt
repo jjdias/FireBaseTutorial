87f8068fb0a171bbe2139bd60084f0b0498fa546
jjdias <jjdias.online@gmail.com>
2019-04-07 15:21:04 -0300

    Added the camera function.

--- app/src/main/java/com/example/firebasetutorial/MainActivity.kt
+++ app/src/main/java/com/example/firebasetutorial/MainActivity.kt
@@ -1,17 +1,46 @@
 package com.example.firebasetutorial
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import kotlinx.android.synthetic.main.activity_main.*
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer
 
 class MainActivity : AppCompatActivity() {
 
+
+    val CAMERA_REQUEST_CODE = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        cameraButton.setOnClickListener{
+            val callCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if(callCamera.resolveActivity(packageManager) != null){
+                startActivityForResult(callCamera, CAMERA_REQUEST_CODE)
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            CAMERA_REQUEST_CODE ->{
+                if (resultCode == Activity.RESULT_OK && data != null){
+                    mainImageView.setImageBitmap(data.extras.get("data") as Bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Req Code desconhecido.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
 
 //    private fun runTextRecognition() {
